003cff426a562d77cd20862e75f454f2ce82539c
bmacer <bmacer@cisco.com>
2021-07-05 01:17:59 -0400

    nn

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Clipperz <code>src/App.js</code> and save to reload.
+          ClipperzZZ <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
